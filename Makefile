9889b8924c28e4a6ea2390a2605266fd320f7e3b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2017-04-24 22:25:13 +0100

    be slack with json parsing

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	docker build -t radio .
 
 run: build
-	docker run -p 5000:5000 --env-file ./env.developmnet -it radio
+	docker run -p 5000:5000 --env-file ./env.development -it radio
 
 remote:
 	cat .git/config | grep '\[remote "dokku"\]' || git remote add dokku dokku@radio.timcowlishaw.co.uk
--- radioserver/views.py
+++ radioserver/views.py
@@ -1,10 +1,11 @@
 from flask import request, jsonify
 from radioserver import app
 from radioserver.models import *
-
+import json
 
 @app.route("/parse_email", methods=["POST"])
 def parse_email():
-    data = request.get_json()
+    # we can't use request.get_json as IFTTT includes newlines in strings which is invalid JSON.
+    data = json.loads(request.data, strict=False)
     print(data)
     return jsonify({'status': 'OK'})
